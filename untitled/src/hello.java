dbcef6396ae0eb50d796dbe287798685070a3650
COCO <80952394+githubwkk@users.noreply.github.com>
2022-07-16 11:26:23 +0800

    Update hello.java

--- untitled/src/hello.java
+++ untitled/src/hello.java
@@ -1,9 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("halo");
-        System.out.println("你好");
-        System.out.println("推送成功测试。");
+       
     }
 }
